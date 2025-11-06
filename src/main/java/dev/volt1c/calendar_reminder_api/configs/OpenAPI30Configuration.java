55936887bfd8c36af83e7817b0e733c71c877c0e
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2025-11-06 17:09:26 +0100

    fix: swagger auth

--- src/main/java/dev/volt1c/calendar_reminder_api/configs/OpenAPI30Configuration.java
+++ src/main/java/dev/volt1c/calendar_reminder_api/configs/OpenAPI30Configuration.java
@@ -1,14 +1,33 @@
 package dev.volt1c.calendar_reminder_api.configs;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SecurityScheme(
-        name = "Bearer Authentication",
+        name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Calendar Reminder API",
+                contact = @Contact(
+                        name = "volt1c",
+                        email = "prgmr.kamil.pawlaczyk@gmail.com",
+                        url = "https://github.com/volt1c"
+                ),
+                license = @License(
+                        name = "MIT License",
+                        url = "https://opensource.org/licenses/MIT"
+                ),
+                description = "Calendar API for task management."
+        )
 )
 public class OpenAPI30Configuration {}
--- src/main/java/dev/volt1c/calendar_reminder_api/configs/SecurityConfiguration.java
+++ src/main/java/dev/volt1c/calendar_reminder_api/configs/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
